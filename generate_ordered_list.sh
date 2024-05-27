3f78cf06669762b96a2b802d673cdb6c52884384
kalonjilab3 <kalonji@fathomdata.dev>
2024-05-27 10:43:42 +0200

    feat: edited script to sort alpahbetically

--- generate_ordered_list.sh
+++ generate_ordered_list.sh
@@ -3,10 +3,15 @@
 directory="chapters"
 output_file="chapter_list.txt"
 
+# Store the first file
 echo "- $directory/Career and Community.qmd" > $output_file
 
+# Find all .qmd files except for the first and last, sort them alphabetically, and append to the output file
 find "$directory" -name "*.qmd" ! -name "Career and Community.qmd" ! -name "Other Compendiums.qmd" |
+sort |
 while read file; do
     echo "- $file" >> $output_file
 done
+
+# Append the last file
 echo "- $directory/Other Compendiums.qmd" >> $output_file
